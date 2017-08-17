a7a0687929634a7e1a294856a9dafcacce9784ce
kaori <kaori55194@gmail.com>
2017-08-17 16:47:02 -0700

    first commit

---
+++ Uppercase/Uppercase/main.m
@@ -0,0 +1,84 @@
+//
+//  main.m
+//  Uppercase
+//
+//  Created by kaori hirata on 2017-08-17.
+//  Copyright © 2017 kaori hirata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        char inputnum[3]; //Number + \n +\0 = 3
+        char inputchars[255];
+        
+        printf("Select Number 1-6\n");
+        fgets(inputnum,3,stdin);
+        NSString *inputNum = [NSString stringWithUTF8String:inputnum];
+        
+        if([inputNum intValue] == 1){
+            //1. Uppercase
+            printf("Input a string(lower):");
+            fgets(inputchars,255,stdin);
+            NSString *inputString = [NSString stringWithCString:inputchars encoding:NSASCIIStringEncoding];
+            NSString *upperString = [inputString uppercaseString];
+            NSLog(@"your string of Upper is %@\n",upperString);
+        }
+        
+        else if([inputNum intValue] == 2){
+        //2. Lowercase
+            printf("Input a string(Upper):");
+            fgets(inputchars,255,stdin);
+            NSString *inputLowerString = [NSString stringWithCString:inputchars encoding:NSASCIIStringEncoding];
+            NSString *lowerString = [inputLowerString lowercaseString];
+            NSLog(@"your string of lower is %@\n",lowerString);
+        }
+        
+        else if([inputNum intValue] == 3){
+            //3. Numberize
+            printf("Input Number:");
+            fgets(inputchars,255,stdin);
+            NSString *inputNumString = [NSString stringWithCString:inputchars encoding:NSASCIIStringEncoding];
+            NSInteger intNum = [inputNumString integerValue];
+            NSLog(@"your Number is %zd\n",intNum);
+        }
+        
+        else if([inputNum intValue] == 4){
+            //4. Canadianize
+            printf("Input Sentence:");
+            fgets(inputchars,255,stdin);
+            NSMutableString *inputSentence = [NSMutableString stringWithCString:inputchars encoding:NSASCIIStringEncoding];
+            [inputSentence insertString:@", eh?" atIndex:5];
+            NSLog(@"Input Sentence + ',eh?' is %@\n",inputSentence);
+        }
+        
+        else if([inputNum intValue] == 5){
+            //5. Respond
+            printf("Input Sententce with mark :");
+            fgets(inputchars,255,stdin);
+            NSString *inputWithMark = [NSString stringWithCString:inputchars encoding:NSASCIIStringEncoding];
+            inputWithMark = [inputWithMark stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            inputWithMark = [inputWithMark stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+            if([inputWithMark hasSuffix:@"?"]== true){
+                NSLog(@"I don't know");
+            }
+            else if ([inputWithMark hasSuffix:@"!"] == true){
+                NSLog(@"Whoa, calm down!");
+            }
+        }
+        
+        else if([inputNum intValue] == 6){
+            //6. De-Space-It
+            printf("Input Sentence with space :");
+            fgets(inputchars,255,stdin);
+            NSString *inputWithSpace = [NSString stringWithCString:inputchars encoding:NSASCIIStringEncoding];
+            inputWithSpace = [inputWithSpace stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+            NSLog(@"input Sentence with space: %@",inputWithSpace);
+        }
+        
+    }
+    return 0;
+}
---
+++ iOS-Delegates-1-FoodTruck-master/Foodtruck/cook.m
@@ -0,0 +1,14 @@
+//
+//  cook.m
+//  Foodtruck
+//
+//  Created by kaori hirata on 2017-08-16.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface Cook : NSObject <FoodTruckDelegate>;
+
+-(BOOL)FoodTruckDelegate {
+    return YES;
+}
