dfa945395d2701e57033932b7e4a9f575356b51a
kaori <kaori55194@gmail.com>
2017-08-17 17:48:34 -0700

    first commit

---
+++ Assignment/Assignment/Method.h
@@ -0,0 +1,13 @@
+//
+//  Method.h
+//  Assignment
+//
+//  Created by kaori hirata on 2017-08-17.
+//  Copyright © 2017 kaori hirata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Method : NSObject
+
+@end
---
+++ Assignment/Assignment/Method.m
@@ -0,0 +1,13 @@
+//
+//  Method.m
+//  Assignment
+//
+//  Created by kaori hirata on 2017-08-17.
+//  Copyright © 2017 kaori hirata. All rights reserved.
+//
+
+#import "Method.h"
+
+@implementation Method
+
+@end
